c507a8bb566084b7228188305df9257aec7a3ed4
Sardorbek8877 <sardorbeksafarov8844@gmail.com>
2023-09-03 16:49:48 +0200

    create Project

---
+++ src/main/java/uz/bek/bankatmcontrolsystem/BankAtmControlSystemApplication.java
@@ -0,0 +1,13 @@
+package uz.bek.bankatmcontrolsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAtmControlSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAtmControlSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/bek/bankatmcontrolsystem/BankAtmControlSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.bek.bankatmcontrolsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAtmControlSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
